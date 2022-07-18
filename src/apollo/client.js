65201d6d1816b3958d2cb149b6460fe1eaf1531d
Christian Martins <christian@labdynamo.com>
2022-07-18 10:57:12 -0700

    removed config/env file

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,11 +1,10 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-import { SETTINGS } from '../config/settings';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: `${SETTINGS.gql_endpoint}`,
+    uri: process.env.REACT_APP_HASURA_ENDPOINT,
     headers: {
-      'x-hasura-admin-secret': `${SETTINGS.hasura_secret}`,
+      "x-hasura-admin-secret": process.env.REACT_APP_HASURA_SECRET,
     },
   }),
   cache: new InMemoryCache(),
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,1 @@
-export const SETTINGS = {
-  gql_endpoint: 'https://true-mustang-60.hasura.app/v1/graphql',
-  hasura_secret:
-    '71EirN0cNK6F2STeTgTHvfQwLur4aWO11aKmXmtHFdLOeuQpRUistxACaGzwsAfY',
-};
+export const SETTINGS = {};
